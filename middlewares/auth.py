6defdd5f00cd16b8e87da2cc63faf227b2f5b1e1
Paul Roman <paul.roman@epitech.eu>
2018-11-11 12:59:16 +0100

    feat: add API authentication

---
+++ middlewares/auth.py
@@ -0,0 +1,26 @@
+import jwt
+from functools import wraps
+from flask_restful import abort, reqparse
+from os import getenv
+from time import time
+
+request_delay = 5
+
+
+def auth_middleware(func):
+    @wraps(func)
+    def func_wrapper(*args, **kwargs):
+        parser = reqparse.RequestParser()
+        parser.add_argument('Authorization', location='headers')
+        try:
+            token = parser.parse_args().__getattr__('Authorization')
+            decoded = jwt.decode(token, getenv('VITRINE_KEY'))
+            request_timestamp = int(decoded.get('date') / 1000)
+            current_timestamp = int(time())
+            if current_timestamp - request_timestamp > request_delay:
+                raise Exception('Request took too long to proceed.')
+        except Exception as e:
+            abort(401, description=str(e))
+        return func(*args, **kwargs)
+
+    return func_wrapper
--- models/game.py
+++ models/game.py
@@ -3,9 +3,12 @@ import os
 from flask_restful import Resource
 
 from lib_wrapper import lib, GoString
+from middlewares.auth import auth_middleware
 
 
 class Game(Resource):
+    method_decorators = [auth_middleware]
+
     def __init__(self):
         super(Game, self).__init__()
         raw_key = os.getenv('IGDB_KEY')
