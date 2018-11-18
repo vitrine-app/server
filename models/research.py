56e66738f58fd416ce9a98f27b8687d285db95ba
Paul Roman <paul.roman@epitech.eu>
2018-11-18 19:55:50 +0100

    fix: rm obsolete env

--- models/research.py
+++ models/research.py
@@ -9,7 +9,5 @@ class ApiResearch(Resource):
     method_decorators = [auth_middleware]
 
     def get(self, game_name):
-        print("ici pignouf omega")
         res = games_catcher.GetFirstGame(GoString(str.encode(game_name), len(game_name)))
-        print(res.decode('utf-8'))
         return {"result": json.loads(res.decode('utf-8'))}
